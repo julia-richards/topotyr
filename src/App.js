f0caad410dab4cea0e6477c0a87dfef66ad6c34c
julia-richards <julia.m.richards@gmail.com>
2019-06-12 16:43:36 -0600

    new create react app site
     Please enter the commit message for your changes. Lines starting

--- src/App.js
+++ src/App.js
@@ -1,25 +1,21 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <main>
+      <div className="title-wrapper">
+        <h1>topotyr</h1>
+        <h4>Maps for avid humans and average adventurers.</h4>
+      </div>
+      <iframe
+        width="100%"
+        height="520"
+        frameborder="0"
+        src="https://juliarichards.carto.com/builder/eae64ec7-73c2-4eb6-af44-90cf5d7c50a3/embed"
+      />
+    </main>
   );
 }
 
