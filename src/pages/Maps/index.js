bd17bd78fd0147d98ec5c2daeb58781c6eb91546
julia-richards <julia.m.richards@gmail.com>
2019-09-29 19:04:20 -0600

    Add title tag

--- src/pages/Maps/index.js
+++ src/pages/Maps/index.js
@@ -10,6 +10,7 @@ function Maps() {
         width="100%"
         height="520"
         frameBorder="0"
+        title="Idaho Mountains"
         src="https://juliarichards.carto.com/builder/eae64ec7-73c2-4eb6-af44-90cf5d7c50a3/embed"
       />
     </React.Fragment>
