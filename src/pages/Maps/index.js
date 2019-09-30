671f393a3e47ca6b458052f83b468433c92b3ec8
julia-richards <julia.m.richards@gmail.com>
2019-09-30 08:53:00 -0600

    Updated map card styles and links

--- src/pages/Maps/index.js
+++ src/pages/Maps/index.js
@@ -1,4 +1,21 @@
 import React from "react";
+import denverMap from "./images/Denver_Maps.png";
+import idahoMtn from "./images/Idaho_Mtns.png";
+import "./Maps.css";
+
+const mapCollection = [
+  {
+    image: denverMap,
+    title: "Denver Neighborhood Map",
+    link: "https://julia-richards.github.io/denver-maps/"
+  },
+  {
+    image: idahoMtn,
+    title: "Idaho Mountains",
+    link:
+      "https://juliarichards.carto.com/builder/eae64ec7-73c2-4eb6-af44-90cf5d7c50a3/embed"
+  }
+];
 
 function Maps() {
   return (
@@ -6,13 +23,16 @@ function Maps() {
       <div className="title-wrapper">
         <h2>Maps</h2>
       </div>
-      <iframe
-        width="100%"
-        height="520"
-        frameBorder="0"
-        title="Idaho Mountains"
-        src="https://juliarichards.carto.com/builder/eae64ec7-73c2-4eb6-af44-90cf5d7c50a3/embed"
-      />
+      <div className="map-card-container">
+        {mapCollection.map((map, i) => (
+          <div className="map-card">
+            <a href={map.link}>
+              <img className="map-card-image" src={map.image} alt={map.title} />
+              <h3 className="map-card-title">{map.title}</h3>
+            </a>
+          </div>
+        ))}
+      </div>
     </React.Fragment>
   );
 }
