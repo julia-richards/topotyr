9733ce364cce58ac36fb716a0b6151efabd5023c
julia-richards <julia.m.richards@gmail.com>
2019-10-09 21:41:06 -0600

    Updates about page

--- src/pages/About/index.js
+++ src/pages/About/index.js
@@ -10,8 +10,21 @@ function About() {
       <div className="About-card">
         <div className="text-wrap">
           <p>
-            <strong>Mission</strong> topotyr makes maps for avid humans and
-            average adventurers.
+            Topotyr is a digital map agency specializing in custom web maps, GIS
+            services, and web design. It is our belief that mapping is an
+            adventure, a catalyst for action and an aesthetic journey. We live
+            and work at the intersection of bringing <strong>topo</strong>graphy
+            to life and adventure, or även<strong>tyr</strong> in Swedish. We’d
+            love to join you on your cartographic adventure, whether it’s
+            creating a beautiful map for your wall or leveraging location
+            intelligence to grow your business.
+          </p>
+          <p>
+            Get in touch{" "}
+            <a href="mailto:julia.m.richards@gmail.com" class="contact-link">
+              here
+            </a>
+            .
           </p>
         </div>
       </div>
